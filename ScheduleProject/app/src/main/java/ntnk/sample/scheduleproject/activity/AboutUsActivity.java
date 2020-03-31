71208ba37be592e711f04e0268372e0a0a3e2197
Hoai Nam <neromix188@gmail.com>
2020-03-31 16:13:55 +0700

    add about us

--- ScheduleProject/app/src/main/java/ntnk/sample/scheduleproject/activity/AboutUsActivity.java
+++ ScheduleProject/app/src/main/java/ntnk/sample/scheduleproject/activity/AboutUsActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,61 +18,82 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Calendar;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 import ntnk.sample.scheduleproject.R;
 
 public class AboutUsActivity extends AppCompatActivity {
 
+    View aboutPage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference();
-
-        final TextView tvIntro = (TextView)findViewById(R.id.tvIntro);
         // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String s = "";
-                for (DataSnapshot keyNode : dataSnapshot.getChildren()){
-                    s = s + keyNode.getValue().toString();
-                }
-                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
-            }
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for (DataSnapshot keyNode : dataSnapshot.getChildren()){
+//                }
+//                //Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//
+//            }
+//        });
+        aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.drawable.app_icon)
+                .setDescription("This is out project in subject PRM391 - Mobile Programing - IS1301")
+                .addItem(new Element().setTitle("Version 1.0"))
+                .addItem(new Element().setTitle("Advertise here"))
+                .addGroup("Connect with us")
+                .addEmail("scheduleprojectttt@gmail.com")
+                .addWebsite("https://github.com/khanhntn13it/prm_schedule_project")
+                .addFacebook("hfs.eurus")
+                .addTwitter("scheduleproject")
+                .addYoutube("UClyA28-01x4z60eWQ2kiNbA")
+                .addPlayStore("com.ideashower.readitlater.pro")
+                .addGitHub("khanhntn13it")
+                .addInstagram("kwonnara_com")
+                .addItem(new Element().setTitle(
+                        String.format("Copyright %d by Schedule Project",
+                                Calendar.getInstance().get(Calendar.YEAR))))
+                .create();
+        setContentView(aboutPage);
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-
-            }
-        });
-
-        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+//        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+//        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
 
-    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    switch (item.getItemId()) {
-                        case R.id.navigation_home:
-                            Intent intent = new Intent(AboutUsActivity.this, BoardActivity.class);
-                            startActivity(intent);
-                            overridePendingTransition(0,0);
-                            return true;
-                        case R.id.navigation_task:
-                            intent = new Intent(AboutUsActivity.this, TodayTaskActivity.class);
-                            startActivity(intent);
-                            overridePendingTransition(0,0);
-                            return true;
-                        case R.id.navigation_aboutus:
-                            intent = new Intent(AboutUsActivity.this, AboutUsActivity.class);
-                            startActivity(intent);
-                            overridePendingTransition(0,0);
-                            return true;
-                    }
-                    return false;
-                }
-            };
+//    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+//            new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                @Override
+//                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                    switch (item.getItemId()) {
+//                        case R.id.navigation_home:
+//                            Intent intent = new Intent(AboutUsActivity.this, BoardActivity.class);
+//                            startActivity(intent);
+//                            overridePendingTransition(0,0);
+//                            return true;
+//                        case R.id.navigation_task:
+//                            intent = new Intent(AboutUsActivity.this, TodayTaskActivity.class);
+//                            startActivity(intent);
+//                            overridePendingTransition(0,0);
+//                            return true;
+//                        case R.id.navigation_aboutus:
+//                            intent = new Intent(AboutUsActivity.this, AboutUsActivity.class);
+//                            startActivity(intent);
+//                            overridePendingTransition(0,0);
+//                            return true;
+//                    }
+//                    return false;
+//                }
+//            };
 }
